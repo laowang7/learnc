bfb979a9b75c7568e75c9bd9deba6b6b952a63ff
laowang7 <34941437+laowang7@users.noreply.github.com>
2023-03-19 21:15:05 +0800

    create  new file
    creat new file in github reporisity

---
+++ one.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int ageq(int a) {
+	int c = 0;
+	if (a == 1) {
+		c = 10;
+		printf("%d\n", c);
+	} else {
+		c = ageq(a - 1) + 2;
+		printf("%d\n", c);
+
+	}
+	return c;
+
+}
+
+int main() {
+	int ageq(int a);
+	ageq(5);
+
+
+	return 0;
+}
